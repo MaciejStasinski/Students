deebe32eefce26dbeaf2d7e81bb108aa3661bad8
Maciej Stasinski <m.stasinski1@gmail.com>
2021-02-15 20:08:22 +0100

    Initial commit

---
+++ src/main/java/com/example/students/controller/StudentController.java
@@ -0,0 +1,95 @@
+package com.example.students.controller;
+
+import com.example.students.model.Student;
+import com.example.students.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody @Valid Student student) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return studentRepository.findById(id).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+//        return studentRepository.findById(id)
+//                .map(student -> {
+//                    studentRepository.delete(student);
+//                    return ResponseEntity.ok().build();
+//                }).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStudent(@PathVariable Long id) {
+        try {
+            studentRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+        }
+    }
+
+    //    @PostMapping ("/{id}")
+//    public Student changeStudent(@RequestBody @Valid Student student) {
+//        return studentRepository.save(student);
+//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> putStudent(@PathVariable Long id,
+                                              @RequestBody @Valid Student student) {
+        return studentRepository.findById(id).map(studentFromDB -> {
+            studentFromDB.setFirstName(student.getFirstName());
+            studentFromDB.setLastName(student.getLastName());
+            studentFromDB.setEmail(student.getEmail());
+            studentRepository.save(studentFromDB);
+            return ResponseEntity.ok().body(studentFromDB);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Student> patchStudent(@PathVariable Long id,
+                                                @RequestBody Student student) {
+        return studentRepository.findById(id)
+                .map(studentFromDB -> {
+                    if (!StringUtils.isEmpty(student.getFirstName())) {
+                        studentFromDB.setFirstName(student.getFirstName());
+                    }
+                    if (!StringUtils.isEmpty(student.getLastName())) {
+                        studentFromDB.setLastName(student.getLastName());
+                    }
+                    if (!StringUtils.isEmpty(student.getEmail())) {
+                        studentFromDB.setEmail(student.getEmail());
+                    }
+                    studentRepository.save(studentFromDB);
+                    return ResponseEntity.ok().body(studentFromDB);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+
+
+    }
+}
---
+++ src/main/java/com/example/students/model/Student.java
@@ -0,0 +1,58 @@
+package com.example.students.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @Email
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
